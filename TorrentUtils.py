3300676e658f36d1c7f6ce1ca7c09c2853131777
ANH KHOA <levananhkhoa2004@gmail.com>
2024-10-19 21:05:27 +0700

    updated

--- TorrentUtils.py
+++ TorrentUtils.py
@@ -1,24 +1,76 @@
 import bencodepy
 import hashlib
 import base64
+import urllib.parse
 
 class TorrentUtils:
 
     @staticmethod
-    def create_torrent_file(meta_info,file_path):
-        encoded_data = meta_info.create_bencode()
+    def get_info_hash_from_file(torrent_file):
+        """
+        Get the hash of info in torrent file.
+        :param torrent_file:
+        :return: hash code
+        """
+        with open(torrent_file, 'rb') as f:
+            file_data = f.read()
+
+        metadata = bencodepy.decode(file_data)
+        subj = metadata[b'info']
+        # Nếu torrent là một directory, sẽ có 'files' thay vì 'length'
+        if b'files' in subj:
+            # Tính tổng kích thước của tất cả các file trong directory
+            total_length = sum(file[b'length'] for file in subj[b'files'])
+        else:
+            # Nếu không phải directory, sử dụng 'length' của file đơn
+            total_length = subj[b'length']
+
+        hash_contents = bencodepy.encode(subj)
+        digest = hashlib.sha1(hash_contents).digest()
+        b32hash = base64.b32encode(digest).decode()
+        return b32hash
+
+    @staticmethod
+    def get_info_hash_from_magnet(magnet_link):
+        # Phân tích (parse) magnet link
+        parsed_link = urllib.parse.urlparse(magnet_link)
+
+        # Tách các tham số query của magnet link
+        query_params = urllib.parse.parse_qs(parsed_link.query)
+
+        # Lấy giá trị của 'xt' (exact topic), trong đó chứa 'urn:btih:<info_hash>'
+        xt_param = query_params.get('xt', [])
+
+        if xt_param:
+            # 'urn:btih:<info_hash>' - chúng ta cần lấy phần <info_hash>
+            xt_value = xt_param[0]
+
+            if xt_value.startswith("urn:btih:"):
+                # Trích xuất và trả về info_hash
+                info_hash = xt_value[9:]  # Bỏ qua "urn:btih:"
+                return info_hash
+        return None
+
+    @staticmethod
+    def create_torrent_file(encoded_data, file_path):
 
         with open(file_path, 'wb') as f:
             f.write(encoded_data)
 
     @staticmethod
-    def make_magnet_from_bencode(torrent):
+    def make_magnet_from_file(file_path):
+        with open(file_path, 'rb') as f:
+            file_data = f.read()
+        return TorrentUtils.make_magnet_from_bencode(file_data)
+
+    @staticmethod
+    def make_magnet_from_bencode(bencode_data):
         """
         Create magnet link from torrent file.
-        :param torrent:
+        :param bencode_data:
         :return: magnet_link
         """
-        metadata = bencodepy.decode(torrent)
+        metadata = bencodepy.decode(bencode_data)
         subj = metadata[b'info']
         # Nếu torrent là một directory, sẽ có 'files' thay vì 'length'
         if b'files' in subj:
@@ -42,8 +94,3 @@ class TorrentUtils:
             + '&tr=' + trackers \
             + '&xl=' + str(total_length)
 
-    @staticmethod
-    def make_magnet_from_file(self, file_path):
-        with open(file_path, 'rb') as f:
-            file_data = f.read()
-        return self.make_magnet_from_encoded(file_data)
--- User.py
+++ User.py
@@ -1,22 +1,44 @@
 from datetime import datetime
 import os
-
 from info import *
 from MetaInfo import MetaInfo
 from TorrentUtils import TorrentUtils
+from Peer import Peer
+import socket
 
 class User:
     def __init__(self, name:str = "Anonymous"):
         self.name = name
+        self.peerList = []
 
-    def input(self):
+    def download(self):
+        isTorrent = False
+        info_hash = ""
+
+        if isTorrent:
+            torrent_file = input("Please input file path: ")
+            info_hash = TorrentUtils.get_info_hash_from_file(torrent_file)
+        else:
+            magnet_link = input("Please input magnet link: ")
+            info_hash = TorrentUtils.get_info_hash_from_magnet(magnet_link)
+
+        ip, port = self._get_ip_port()
+        peer = Peer(ip, port)
+
+
+
+    def share(self):
+        magnet_link = self._input()
+        return magnet_link
+
+    def _input(self):
         path = input("Nhập vào đường dẫn đến file hoặc directory: ")
         if os.path.isdir(path):
-            self._input_directory(path)
+            return self._input_directory(path)
         elif os.path.isfile(path):
-            self._input_file(path)
+            return self._input_file(path)
         elif not os.path.exists(path):
-            print("Invalid path")
+            raise "Invalid path"
 
     def _input_directory(self, dir_path):
         """
@@ -40,8 +62,11 @@ class User:
         # Tạo MetaInfo cho torrent file
         meta_info = MetaInfo(info, 'http://localhost:8080', datetime.now(), 'No comment', self.name)
         encoded = meta_info.get_bencode()
+        TorrentUtils.create_torrent_file(encoded, 'download.torrent')
         magnet_link = TorrentUtils.make_magnet_from_bencode(encoded)
-        print(magnet_link)
+
+        return magnet_link
+
 
     def _input_file(self, file_path):
         """
@@ -59,10 +84,24 @@ class User:
         # Tạo MetaInfo cho torrent file
         meta_info = MetaInfo(info, 'http://localhost:8080', datetime.now(), 'No comment', self.name)
         encoded = meta_info.get_bencode()
+        TorrentUtils.create_torrent_file(encoded, 'download.torrent')
         magnet_link = TorrentUtils.make_magnet_from_bencode(encoded)
-        print(magnet_link)
 
+        return magnet_link
+
+    def _get_ip_port(self):
+        """Lấy địa chỉ IP và tìm một cổng trống cho Peer."""
+        # Lấy địa chỉ IP của máy người dùng
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+
+        # Tìm một port trống để dùng
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('', 0))  # Bind tới bất kỳ cổng trống nào (0 là chỉ định hệ thống tự tìm port)
+            port = s.getsockname()[1]  # Lấy port đã bind
+        return ip_address, port
 
 if __name__ == '__main__':
     user = User()
-    user.input()
+    magnet_link = user.share()
+    print("Magnet link:", magnet_link)
